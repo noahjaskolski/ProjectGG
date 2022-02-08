21ddf8e2422ef557c223e2444cebe294dbb6c494
siowlim13 <92942519+siowlim13@users.noreply.github.com>
2022-02-07 17:03:25 -0700

    added WIP API

---
+++ Api/Models/Post.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+//create schema
+const PostSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+
+    lastName: {
+        type: String,
+        required: true
+    },
+
+    email: {
+        type: String,
+        required: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+    level: String
+}, {collection: 'users_info'});
+
+module.exports = mongoose.model('Posts', PostSchema);
